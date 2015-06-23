e4627507f876b0451c8086e9f807fc09c335a6f7
Schmavery <elflord@live.ca>
2015-06-23 07:20:11 -0400

    Add library for bot development when offline

--- cli.js
+++ cli.js
@@ -3,6 +3,8 @@ var stdin = process.openStdin();
 var lisp = require("./lisp");
 
 lisp.evaluate(lisp.parse("(load std-lib)"));
+lisp.evaluate(lisp.parse("(load polyfill)"));
+lisp.evaluate(lisp.parse("(load bot-lib)"));
 
 stdin.addListener("data", function(d) {
   var str = d.toString().substring(0, d.length-1).trim();
